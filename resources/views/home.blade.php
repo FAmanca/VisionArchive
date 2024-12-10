2609cf61d5c3cce4c13da03fc326c8972a9f64b0
FAmanca <fadlyamanca@gmail.com>
2024-12-10 23:30:51 +0700

    Refactor & Add : Loading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,24 +55,25 @@
 
             {{-- New Arrivals Section --}}
             <h2 class="mb-4">New Arrivals</h2>
-            <div class="grid">
-                @foreach ($newimages as $newimage)
-                    <div class="grid-item">
-                        <a href="{{ route('image.show', $newimage->id) }}" class="card-link">
-                            <div class="card">
-                                <img src="{{ asset('storage/' . $newimage->foto) }}" class="card-img-top" alt="Image">
-                                {{-- Card Buttons --}}
-                                <div class="card-body text-center">
-                                    <button class="btn btn-like"><i class="fa fa-heart"></i></button>
-                                    <a href="{{ route('image.download', $newimage->id) }}" class="btn btn-download"><i
-                                            class="fa fa-download"></i></a>
-                                    <button class="btn btn-share"><i class="fa fa-share"></i></button>
+                <div class="grid">
+                    @foreach ($newimages as $newimage)
+                        <div class="grid-item">
+                            <a href="{{ route('image.show', $newimage->id) }}" class="card-link">
+                                <div class="card">
+                                    <img src="{{ asset('storage/' . $newimage->foto) }}" class="card-img-top"
+                                        alt="Image">
+                                    {{-- Card Buttons --}}
+                                    <div class="card-body text-center">
+                                        <button class="btn btn-like"><i class="fa fa-heart"></i></button>
+                                        <a href="{{ route('image.download', $newimage->id) }}" class="btn btn-download"><i
+                                                class="fa fa-download"></i></a>
+                                        <button class="btn btn-share"><i class="fa fa-share"></i></button>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-            </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
 
 
             {{-- Pagination (Dummy) --}}
@@ -87,15 +88,51 @@
         @endif
 
     </div>
+    <span class="loader"></span>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/4.1.4/imagesloaded.pkgd.min.js"></script>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
+            var content = document.querySelector('.container');
+            var load = document.querySelector('.loader');
+            content.style.display = 'none';
+
             var grid = document.querySelector('.grid');
-            var msnry = new Masonry(grid, {
-                itemSelector: '.grid-item',
-                columnWidth: '.grid-item',
-                percentPosition: true
+            var images = grid.querySelectorAll('img');
+            var imagesLoaded = 0;
+            var totalImages = images.length;
+
+            // Fungsi untuk mengecek jika semua gambar telah dimuat
+            function onImageLoad() {
+                imagesLoaded++;
+                if (imagesLoaded === totalImages) {
+                    // Semua gambar sudah dimuat, jalankan kode berikutnya
+                    content.style.display = 'block';
+                    load.style.display = 'none';
+
+                    // Inisialisasi Masonry
+                    var msnry = new Masonry(grid, {
+                        itemSelector: '.grid-item',
+                        columnWidth: '.grid-item',
+                        percentPosition: true
+                    });
+                }
+            }
+
+            // Menambahkan event listener untuk setiap gambar
+            images.forEach(function(image) {
+                image.addEventListener('load', onImageLoad);
+
+                // Jika gambar sudah ada di cache, langsung hitung sebagai dimuat
+                if (image.complete) {
+                    onImageLoad();
+                }
             });
+
+            // Jika tidak ada gambar, langsung jalankan
+            if (totalImages === 0) {
+                onImageLoad();
+            }
         });
     </script>
 @endsection
